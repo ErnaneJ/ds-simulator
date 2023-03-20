351c4afd0f1adb54a98b1053aff089dff48f797f
Raphael Ramos <raphael.opensource@gmail.com>
2023-03-20 20:06:35 -0300

    feat: double click to remove node

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -11,13 +11,13 @@ import * as Add from '../lib/Add';
 import * as Remove from '../lib/Remove';
 import * as Errors from '../lib/Errors';
 
-import ReactFlow, { 
-  addEdge, Background, Connection, ConnectionMode, Controls, 
-  Node, useEdgesState, useNodesState 
+import ReactFlow, {
+  addEdge, Background, Connection, ConnectionMode, Controls,
+  Node, useEdgesState, useNodesState
 } from 'reactflow';
 
 import 'reactflow/dist/style.css';
-import {ToastNotification} from './ToastNotification';
+import { ToastNotification } from './ToastNotification';
 
 export default function Canvas() {
   const [objectType, setObjectType] = useState('arrayListElement');
@@ -39,27 +39,27 @@ export default function Canvas() {
     setEdges([]);
     setNodes((nds) => {
       return nds.map((node, i) => {
-        if(objectType === 'arrayListElement') node.data = { 
-          ...node.data, index: i, 
-          last: nds.length === i+1, 
-          label: `0x${i.toString(16).padStart(4,'0')}`
+        if (objectType === 'arrayListElement') node.data = {
+          ...node.data, index: i,
+          last: nds.length === i + 1,
+          label: `0x${i.toString(16).padStart(4, '0')}`
         };
 
-        if(objectType === 'linkedListNode') node.data = {
-           ...node.data, index: i, 
-           next: nds[i+1]?.data.label 
+        if (objectType === 'linkedListNode') node.data = {
+          ...node.data, index: i,
+          next: nds[i + 1]?.data.label
         };
 
-        if(objectType === 'doubleLinkedListNode') node.data = { 
-          ...node.data, 
-          next: nds[i+1]?.data.label, 
-          prev: nds[i-1]?.data.label 
+        if (objectType === 'doubleLinkedListNode') node.data = {
+          ...node.data,
+          next: nds[i + 1]?.data.label,
+          prev: nds[i - 1]?.data.label
         };
 
         const source = node;
         const target = nds[i + 1];
-        if(source && target){
-          let defaultConnection:any = {
+        if (source && target) {
+          let defaultConnection: any = {
             "type": "default",
             "source": source.id,
             "sourceHandle": "right",
@@ -70,15 +70,15 @@ export default function Canvas() {
             "animated": true,
           };
 
-          if(objectType === 'arrayListElement'){
+          if (objectType === 'arrayListElement') {
             delete defaultConnection["markerEnd"];
             delete defaultConnection["markerStart"];
             delete defaultConnection["animated"];
-          }else if(objectType === 'linkedListNode'){
-            defaultConnection = { ...defaultConnection,  "targetHandle": "bottom" } 
-          }else if(objectType === 'doubleLinkedListNode'){
+          } else if (objectType === 'linkedListNode') {
+            defaultConnection = { ...defaultConnection, "targetHandle": "bottom" }
+          } else if (objectType === 'doubleLinkedListNode') {
             const inverseConnection = {
-              "type": "default", 
+              "type": "default",
               "source": target.id,
               "sourceHandle": "left",
               "target": source.id,
@@ -89,24 +89,24 @@ export default function Canvas() {
             };
             setEdges(edges => addEdge(inverseConnection, edges));
           }
-          
+
           setEdges(edges => addEdge(defaultConnection, edges));
         }
-    
+
         return node;
       })
     });
-  }, [nodes, edges]);
+  }, [nodes, edges, indexToChange]);
 
-  function addNode(){
-    if(Errors.objectType({
+  function addNode() {
+    if (Errors.objectType({
       objectType,
       setToastOpen,
       setTitleToast,
       setDescriptionToast
     })) return;
 
-    if(Errors.invalidIndex({
+    if (Errors.invalidIndex({
       indexToChange,
       nodesLength: nodes.length,
       setToastOpen,
@@ -114,23 +114,23 @@ export default function Canvas() {
       setDescriptionToast
     })) return;
 
-    if(!indexToChange) setIndexToChange(-1);
+    if (!indexToChange) setIndexToChange(-1);
 
     const refNode = indexToChange != -1 ? nodes[indexToChange] : nodes[nodes.length - 1];
     const sizesNode = (SIZES_NODES as any)[objectType];
     const yPos = (refNode?.position.y || 200) + (nodes.length % 2 == 0 ? -1 : 1) * sizesNode.height;
 
-    if(objectType === 'arrayListElement') Add.FromArrayList({
-      refNode, yPos, nodes,
-      indexToChange, sizesNode, setNodes
-    });
-    
-    if(objectType === 'linkedListNode') Add.FromLinkedList({
+    if (objectType === 'arrayListElement') Add.FromArrayList({
       refNode, yPos, nodes,
       indexToChange, sizesNode, setNodes
     });
 
-    if(objectType === 'doubleLinkedListNode') Add.FromDoubleLinkedList({
+    if (objectType === 'linkedListNode') Add.FromLinkedList({
+      refNode, yPos, nodes,
+      indexToChange, sizesNode, setNodes
+    });
+
+    if (objectType === 'doubleLinkedListNode') Add.FromDoubleLinkedList({
       refNode, yPos, nodes,
       indexToChange, sizesNode, setNodes
     });
@@ -138,15 +138,17 @@ export default function Canvas() {
     autoLayout();
   }
 
-  function removeNode(){
-    if(Errors.objectType({
+
+  function removeNode() {
+
+    if (Errors.objectType({
       objectType,
       setToastOpen,
       setTitleToast,
       setDescriptionToast
     })) return;
 
-    if(Errors.invalidIndex({
+    if (Errors.invalidIndex({
       indexToChange,
       nodesLength: nodes.length,
       setToastOpen,
@@ -159,20 +161,36 @@ export default function Canvas() {
       nodes, indexToChange, setNodes, sizesNode
     }
 
-    if(objectType === 'arrayListElement') Remove.FromArrayList(paramsToRemove);
-    if(objectType === 'linkedListNode') Remove.FromLinkedList(paramsToRemove);
-    if(objectType === 'doubleLinkedListNode') Remove.FromDoubleLinkedList(paramsToRemove);
+    if (objectType === 'arrayListElement') Remove.FromArrayList(paramsToRemove);
+    if (objectType === 'linkedListNode') Remove.FromLinkedList(paramsToRemove);
+    if (objectType === 'doubleLinkedListNode') Remove.FromDoubleLinkedList(paramsToRemove);
 
     autoLayout();
   }
-  
-  function autoLayout(){
+
+  function handleNodeClick(event: React.MouseEvent, node: Node) {
+    switch (event.detail) {
+      case 1: {
+        break;
+      }
+      case 2: {
+        setIndexToChange(parseInt(node.data.index));
+        removeNode();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  function autoLayout() {
     if (!autoLayoutToggle) return;
-    setNodes((nds:Node[]) => {
+    setNodes((nds: Node[]) => {
       return nds.map((node, i) => {
         const lastNode = nds[i - 1];
         node.position = {
-          x: (lastNode?.position.x || 100) + (SIZES_NODES as any)[objectType].width, 
+          x: (lastNode?.position.x || 100) + (SIZES_NODES as any)[objectType].width,
           y: (lastNode?.position.y || 100) + (i % 2 == 0 ? -1 : 1) * (SIZES_NODES as any)[objectType].height
         };
         return node;
@@ -180,7 +198,7 @@ export default function Canvas() {
     });
   }
 
-  const changeListType = (type:string) => {
+  const changeListType = (type: string) => {
     setObjectType(type);
     setNodes(loadDefaltNode(type));
     setEdges([]);
@@ -188,7 +206,7 @@ export default function Canvas() {
   }
 
   return (
-    <div className="w-screen h-screen bg-neutral-900">       
+    <div className="w-screen h-screen bg-neutral-900">
       <ReactFlow
         nodeTypes={NODE_TYPES}
         edgeTypes={EDGE_TYPES}
@@ -197,16 +215,17 @@ export default function Canvas() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         onNodesChange={onNodesChange}
+        onNodeClick={handleNodeClick}
         connectionMode={ConnectionMode.Loose}
         defaultEdgeOptions={{
           type: 'default'
         }}
       >
         <Background gap={12} size={2} color={neutral[800]} />
-        <Controls style={{backgroundColor: neutral[50]}}/>
+        <Controls style={{ backgroundColor: neutral[50] }} />
       </ReactFlow>
 
-      <SwitchAutoLayout 
+      <SwitchAutoLayout
         autoLayoutToggle={autoLayoutToggle}
         setAutoLayoutToggle={setAutoLayoutToggle}
       />
@@ -216,14 +235,14 @@ export default function Canvas() {
         changeListType={changeListType}
       />
 
-      <ToolbarFooter 
-        addNode={addNode} 
-        removeNode={removeNode} 
-        indexToChange={indexToChange} 
-        setIndexToChange={setIndexToChange} 
+      <ToolbarFooter
+        addNode={addNode}
+        removeNode={removeNode}
+        indexToChange={indexToChange}
+        setIndexToChange={setIndexToChange}
       />
 
-      <ToastNotification 
+      <ToastNotification
         open={toastOpen}
         setOpen={setToastOpen}
         title={titleToast}
